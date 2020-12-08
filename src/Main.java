2cb48661bf2fc43a5484bcab33b6f816f466aa1e
Nadine09 <64920054+Nadine09@users.noreply.github.com>
2020-12-08 20:09:17 +0100

    Update Main.java
    Añadido el método imprimeSaludo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 
 public class Main {
+	
+	public static void imprimeSaludo() { 
+		System.out.println(“Hei”);	
+	}
+
 	public static void main(String []args) {
 		System.out.println("Hola mundo");
 	}
